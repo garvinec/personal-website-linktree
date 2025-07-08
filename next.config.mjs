109d42ffb366ccb24ba69116f20536ba5e66eb3b
garvinec <chan.gar@northeastern.edu>
2025-07-09 01:38:18 +0800

    modifies basepath in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  basePath: isProd ? "/personal-website-linktree" : "",
   output: "export",
-  basePath: "/personal-website-linktree",
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
